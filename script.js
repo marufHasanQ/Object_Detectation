dd19a3a9f846464f83cad64068318ce3a12170d5
marufHasanQ <45353886+marufHasanQ@users.noreply.github.com>
2020-04-29 17:59:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,176 +1,170 @@
-let video ;
-  let canvas ;
-  let pre ;
-  let model = null;
-  let predictions;
-  let stream;
-  let predict=false;
-  let loadPre;
-  let wikipages;
+let video;
+let canvas;
+let pre;
+let model = null;
+let predictions;
+let stream;
+let predict = false;
+let loadPre;
+let wikipages;
 let imageUpload;
+let smallCanvas;
+
 function setup() {
-   video = document.getElementById("video");
-   canvas = document.getElementById("canvas");
-   pre = document.getElementById("predictions");
-   model = null;
-   video = document.getElementById("video");
- loadPre = document.getElementById("loadPre");
-  wikipages=document.getElementById("wikipages");
-  imageUpload=document.getElementById("imageUpload").addEventListener('change',handleImageUpload, false);;
-  
-  
- 
+  video = document.getElementById("video");
+  canvas = document.getElementById("canvas");
+  pre = document.getElementById("predictions");
+  model = null;
+  video = document.getElementById("video");
+  loadPre = document.getElementById("loadPre");
+  wikipages = document.getElementById("wikipages");
 
-
-
-  
-  
+  imageUpload = document
+    .getElementById("imageUpload")
+    .addEventListener("change", handleImageUpload, false);
 
   async function main() {
-  //   try {
-  //     model = await mobilenet.load();
-      
-  // } catch (e) {
-  //     console.log("model is not loading"+e);
+    //   try {
+    //     model = await mobilenet.load();
 
-  // }
-  // finally{
-  //   console.log("it loaded")
-  // }
-  //   console.log("new")
+    // } catch (e) {
+    //     console.log("model is not loading"+e);
 
-  mobilenet.load()
-  .then((result)=>{model=result;
-    loadPre.innerHTML='model loaded';
-                   
-  })
-  .catch ((e)=>{
-    loadPre.innerHTML='Model failed to load';
-    console.log("model is not loading"+e)
-  }
-        );
-  
-    
-      
-    
-    
+    // }
+    // finally{
+    //   console.log("it loaded")
+    // }
+    //   console.log("new")
+
+    mobilenet
+      .load()
+      .then((result) => {
+        model = result;
+        loadPre.innerHTML = "model loaded";
+
+        document.getElementsByClassName("first")[0].style.backgroundColor =
+          "green";
+      })
+      .catch((e) => {
+        loadPre.innerHTML = "Model failed to load";
+        console.log("model is not loading" + e);
+      });
   }
   main();
-  
 }
-async function loadModel(){
-  loadPre.innerHTML='Model loading...again';
-  mobilenet.load()
-  .then((result)=>{model=result;
-    loadPre.innerHTML='model loaded';
-  })
-  .catch ((e)=>{
-    loadPre.innerHTML='Model failed to load';
-    console.log("model is not loading"+e)
-  }
-        );
-  
-  
+async function loadModel() {
+  loadPre.innerHTML = "Model loading...again";
+  mobilenet
+    .load()
+    .then((result) => {
+      model = result;
+      loadPre.innerHTML = "model loaded";
+    })
+    .catch((e) => {
+      loadPre.innerHTML = "Model failed to load";
+      console.log("model is not loading" + e);
+    });
 }
 
 async function classifyImage() {
-  
   predictions = await model.classify(canvas);
   displayPredictions(predictions);
 }
 
 function displayPredictions(predictions) {
- //window.alert(predictions[0].className); 
+  //window.alert(predictions[0].className);
 
   let val = "";
 
   for (prediction of predictions) {
     let perc = (prediction.probability * 100).toFixed(2);
     val += `${perc}% | ${prediction.className}\n`;
-    
   }
-  
+
   pre.innerHTML = val;
 }
- async function handleImageUpload(e){
-    var reader = new FileReader();
-  reader.readAsDataURL(e.target.files[0]);
-    reader.onload = function(event){
-      let context = canvas.getContext("2d");
-        var img = new Image();
-        img.onload = function(){
-            canvas.width = img.width;
-            canvas.height = img.height;
-            context.drawImage(img,0,0);
-        }
-        img.src = event.target.result;
+
+function speakPrediction() {
+  let voiceSpeaker;
+  speechSynthesis.getVoices().forEach(function (voice, index) {
+    console.log(voice.name);
+    if (voice.name == "Google हिन्दी") {
+      console.log("y");
     }
-   const r = await model.classify(canvas);
+    voiceSpeaker = voice;
+  });
+}
+
+async function handleImageUpload(e) {
+  var reader = new FileReader();
+  reader.readAsDataURL(e.target.files[0]);
+  reader.onload = function (event) {
+    let context = canvas.getContext("2d");
+    var img = new Image();
+    img.onload = function () {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      context.drawImage(img, 0, 0);
+    };
+    img.src = event.target.result;
+  };
+  const r = await model.classify(canvas);
   console.log(r);
-  
+
   classifyImage();
-         
 }
 function takeSnapshot() {
   let context = canvas.getContext("2d"),
     width = video.videoWidth,
     height = video.videoHeight;
-    
+
   if (width && height) {
     // Setup a canvas with the same dimensions as the video.
-    canvas.width =width;
-    canvas.height =height;
+    canvas.width = width;
+    canvas.height = height;
 
     // Make a copy of the current frame in the video on the canvas.
     context.drawImage(video, 0, 0, width, height);
-    if(predict){
+    if (predict) {
       classifyImage();
     }
-      
-    
-    
   }
 }
 
-function showDetails(){
-    
-   let a =predictions[0].className;
+function showDetails() {
+  let a = predictions[0].className;
   //window.alert(a);
 
-let b=a.split(',');let c=b[0].charAt(0).toUpperCase()+b[0].slice(1).replace(' ','_');
+  let b = a.split(",");
+  let c = b[0].charAt(0).toUpperCase() + b[0].slice(1).replace(" ", "_");
 
-  wikipages.src="https://en.wikipedia.org/wiki/"+c;
-  
-  
+  wikipages.src = "https://en.wikipedia.org/wiki/" + c;
 }
 
-function predictToggle(){
-  predict=!predict;
+function predictToggle() {
+  predict = !predict;
   predictButton = document.getElementById("predictButton");
-  predictButton.innerHTML = predict?'Stop Predicting':'Start Predicting';
+  predictButton.innerHTML = predict ? "Stop Predicting" : "Start Predicting";
 }
 async function startCamera() {
   try {
-    
-      stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      console.log("mediaDevices");
-} catch(err) {
-  console.log("mediaDevices error"+err); // TypeError: failed to fetch
-}
+    stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    console.log("mediaDevices");
+  } catch (err) {
+    console.log("mediaDevices error" + err); // TypeError: failed to fetch
+  }
   if ("srcObject" in video) {
     video.srcObject = stream;
     console.log("src object");
   } else {
     console.log("no src object");
   }
-  
+
   try {
     await video.play();
-  } catch(err) {
-    
-  console.log(err);
-}
-  
+  } catch (err) {
+    console.log(err);
+  }
 
   setInterval(() => takeSnapshot(), 3000);
 }
